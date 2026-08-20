ea951b0dcef5fa6cb026a532b2ddd7c7da05ae63
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-08-20 18:51:31 +0800

    Fix measurement benchmark import order

--- src/bayesian_ach/measurement_benchmark.py
+++ src/bayesian_ach/measurement_benchmark.py
@@ -3,7 +3,7 @@
 from __future__ import annotations
 
 from dataclasses import asdict, dataclass
-from typing import Any, Final, TYPE_CHECKING
+from typing import TYPE_CHECKING, Any, Final
 
 import numpy as np
 from numpy.typing import NDArray
