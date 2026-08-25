b9d0bc46157c320bdef318232318005943251c10
Florian Pfaff <6773539+FlorianPfaff@users.noreply.github.com>
2026-08-25 10:14:22 +0800

    Fix certified design API exports

--- src/bayesian_ach/design.py
+++ src/bayesian_ach/design.py
@@ -1,6 +1,12 @@
 """Public prospective experimental-design API."""
 
-from bayesian_ach.design_certificate import (\n    CertifiedDesignConfig,\n    MaximinCertificate,\n    certificate_matches_geometry,\n    certify_maximin_design,\n)\nfrom bayesian_ach.design_geometry import (
+from bayesian_ach.design_certificate import (
+    CertifiedDesignConfig,
+    MaximinCertificate,
+    certificate_matches_geometry,
+    certify_maximin_design,
+)
+from bayesian_ach.design_geometry import (
     DesignDiagnostics,
     design_diagnostics,
     pairwise_residual_matrix,
@@ -16,10 +22,14 @@ from bayesian_ach.design_grid import (
 from bayesian_ach.design_optimizer import OptimizedDesign, optimize_maximin_design
 
 __all__ = [
+    "CertifiedDesignConfig",
     "DESIGN_CANDIDATE_NAMES",
     "DesignDiagnostics",
+    "MaximinCertificate",
     "OptimizedDesign",
     "TransitionDesignGridConfig",
+    "certificate_matches_geometry",
+    "certify_maximin_design",
     "coupled_novelty_design",
     "design_diagnostics",
     "generate_transition_design_grid",
